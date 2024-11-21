de2f2797536e48ab00699ff1dbe86f55d6b241b8
[Mainak Banduri] <[mainak407@gmail.com]>
2024-11-22 01:19:26 +0530

    full complete but api limit expire

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,41 +1,43 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import {API_KEY} from '/@env';
+// import { API_KEY } from "@/env";
+// import { API_KEY } from "@env";
+// console.log(API_KEY);
+
+// const rapidAPI=API_KEY;
+console.log('ppppppp');
+
+console.log(process.env.API_KEY);
+console.log('xxxxxxxxxx');
+
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-console.log(process.env.API_KEY);
-// console.log(API_KEY);
 
   const options = {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-key": `${process.env.API_KEY}`,
       "x-rapidapi-host": "jsearch.p.rapidapi.com",
     },
-    params: {
-      ...query,
-    },
+    params: { ...query },
   };
 
   const fetchData = async () => {
-    setIsLading(true);
-
+    setIsLoading(true);
     try {
       const response = await axios.request(options);
-
-      setData(response.data.data);
-      setIsLading(false);
+      setData(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error(error);
-      
       setError(error);
       alert("There is an error");
     } finally {
-      setIsLading(false);
+      setIsLoading(false);
     }
   };
 
@@ -43,21 +45,7 @@ console.log(process.env.API_KEY);
     fetchData();
   }, []);
 
-  const reFetch = () => {
-    setIsLading(true);
-    fetchData();
-  };
-
-  return { data, isLoading, error, reFetch };
+  return { data, isLoading, error, reFetch: fetchData };
 };
 
-export default useFetch
-// import React from 'react'
-
-// const useFetch = () => {
-//   return (
-//     <div>useFetch</div>
-//   )
-// }
-
-// export default useFetch
+export default useFetch;
